ab91705cd66fccca6f69c999c96d57d36efa4dbe
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-13 15:34:48 +0300

    test первая программа Hello_world
    Для проверки настроек компонентов среды разработки
    первая программа Hello_world.
    Добавлен

---
+++ sendbox/src/main/java/FirstProg1.java
@@ -0,0 +1,13 @@
+public class FirstProg1 {
+
+public static void main ( String[] args) {
+// простая программа для проверки сборщика и системы контроля версий
+		
+		System.out.println ();
+		System.out.println ( "******_TEST_*************");
+		System.out.println ("     " + "Hello, world !");
+		System.out.println ("******_TEST_*************");
+
+}
+
+}
