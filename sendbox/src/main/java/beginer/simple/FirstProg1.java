6706db7dca66b00b6a7eb75dcb9b7cda58e33deb
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-14 13:27:28 +0300

    добавлен метод (функция) hello

--- sendbox/src/main/java/beginer/simple/FirstProg1.java
+++ sendbox/src/main/java/beginer/simple/FirstProg1.java
@@ -7,14 +7,16 @@ public class FirstProg1 {
 
 	public static void main ( String[] args) {
 // простая программа для проверки сборщика и системы контроля версий
-		LocalDateTime testtime = LocalDateTime.now();
-		/*  эксперимент с лябда выражением */
+		hello();
+	}
 
+	public static void hello () {
+		// заполненени переменой значением текущей даты и времени
+		LocalDateTime testtime = LocalDateTime.now();
+		// вывод сообщения в консоль
 		System.out.println ( "******_TEST_START*************");
 		System.out.println ( "   "  + testtime + "    " );
 		System.out.println ("     " + "Hello, world !"  + "     ");
 		System.out.println ("******_TEST_END*************");
-
 	}
-
 }
