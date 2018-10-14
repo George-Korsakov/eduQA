c46f2e0e14ec9188003d6665fc240e390ad4999b
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-14 22:21:23 +0300

    Update sendbox/src/main/java/beginer/simple/FirstProg1.java

--- sendbox/src/main/java/beginer/simple/FirstProg1.java
+++ sendbox/src/main/java/beginer/simple/FirstProg1.java
@@ -7,16 +7,31 @@ public class FirstProg1 {
 
 	public static void main ( String[] args) {
 // простая программа для проверки сборщика и системы контроля версий
-		hello();
+		hello("world");
+		hello("test");
+		char a = 25;
+		double b = 33.0;
+		// пример вызова функции с неявным преобразованием типов
+		System.out.println( area( b, a));
 	}
 
-	public static void hello () {
+
+
+	public static void hello (String somebody) {
 		// заполненени переменой значением текущей даты и времени
 		LocalDateTime testtime = LocalDateTime.now();
 		// вывод сообщения в консоль
 		System.out.println ( "******_TEST_START*************");
 		System.out.println ( "   "  + testtime + "    " );
-		System.out.println ("     " + "Hello, world !"  + "     ");
+		System.out.println ("     " + "Hello, "+ somebody + " !"  + "     ");
 		System.out.println ("******_TEST_END*************");
 	}
+	// расчет площади
+	public static double area (double l1, double l2){
+		double summ = l1 * l2;
+		return summ;
+	}
+
+
+
 }
