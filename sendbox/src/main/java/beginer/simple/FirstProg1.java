43e37225a56d7abfc63b6671649310b47c84fa2d
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-15 23:27:07 +0300

    исправлено форматирование кода

--- sendbox/src/main/java/beginer/simple/FirstProg1.java
+++ sendbox/src/main/java/beginer/simple/FirstProg1.java
@@ -2,37 +2,31 @@ package beginer.simple;
 
 import java.time.LocalDateTime;
 
-
 public class FirstProg1 {
 
-	public static void main ( String[] args) {
+  public static void main(String[] args) {
 // простая программа для проверки сборщика и системы контроля версий
-		int a = 25;
-		double b = 33.0;
-		hello("world");
-		System.out.println( "Плошадь прямоугольнка с сторанми " + a + " и " + b + " равна " +  area( b, a));
-		// пример вызова функции с неявным преобразованием типов
+    int a = 25;
+    double b = 33.0;
+    hello("world");
+    System.out.println("Плошадь прямоугольнка со сторанми " + a + " и " + b + " равна " + area(b, a));
+    // пример вызова функции с неявным преобразованием типов
+  }
 
-	}
+  public static void hello(String somebody) {
+    // заполненени переменой значением текущей даты и времени
+    LocalDateTime testtime = LocalDateTime.now();
+    // вывод сообщения в консоль
+    System.out.println("******_TEST_START*************");
+    // выводить текущую дату и время
+    System.out.println("   " + testtime + "    ");
+    System.out.println("     " + "Hello, " + somebody + " !" + "     ");
+    System.out.println("******_TEST_END*************");
+  }
 
-
-
-	public static void hello (String somebody) {
-		// заполненени переменой значением текущей даты и времени
-		LocalDateTime testtime = LocalDateTime.now();
-		// вывод сообщения в консоль
-		System.out.println ( "******_TEST_START*************");
-		// выводить текущую дату и время
-		System.out.println ( "   "  + testtime + "    " );
-		System.out.println ("     " + "Hello, "+ somebody + " !"  + "     ");
-		System.out.println ("******_TEST_END*************");
-	}
-	// расчет площади
-	public static double area (double l1, double l2){
-		double summ = l1 * l2;
-		return summ;
-	}
-
-
+  // расчет площади
+  public static double area(double l1, double l2) {
+    return l1 * l2;
+  }
 
 }
