fa86183147567b6d89590003d2f19ac4bf1c7d2c
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-17 02:09:45 +0300

    функция distance переделана в метод класса Point
    Задание 2

--- sendbox/src/main/java/beginer/simple/FirstProg1.java
+++ sendbox/src/main/java/beginer/simple/FirstProg1.java
@@ -8,11 +8,11 @@ public class FirstProg1 {
 // простая программа для проверки сборщика и системы контроля версий
     hello("world");
     // создаем обхкты точки и задаем им значение параметров
-    Point p1 = new Point(9, 8);
-    Point p2 = new Point(6, 4);
+    Point p1 = new Point(5, 7);
+    Point p2 = new Point(8, 3);
 
     // вызываем функцию расчета расстояние между точками и выводим результат вычислений
-    System.out.println("Расстояние моежду nочками " + "(" + p1.x + ", " + p1.y + ") и (" + p2.x + ", "+ p2.y + ") равнo " + distance(p1, p2));
+    System.out.println("Расстояние моежду nочками " + "(" + p1.x + ", " + p1.y + ") и (" + p2.x + ", "+ p2.y + ") равнo " + p1.distance(p2.x, p2.y));
     // пример вызова функции с неявным преобразованием типов
   }
 
@@ -25,11 +25,6 @@ public class FirstProg1 {
     System.out.println("   " + testtime + "    ");
     System.out.println("     " + "Hello, " + somebody + " !" + "     ");
     System.out.println("******_TEST_END*************");
-  }
-
-  // функция которая вычисляет расстояение между двумя точками
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p1.x-p2.x),2)+Math.pow((p1.y-p2.y),2));
   }
 
 }
--- sendbox/src/main/java/beginer/simple/Point.java
+++ sendbox/src/main/java/beginer/simple/Point.java
@@ -6,7 +6,12 @@ public class Point {
  public Point(int x, int y){
    this.x = x;
    this.y = y;
-
  }
 
+
+  // вычисление расстояение между двумя точками
+  public double distance(int x2, int y2) {
+   return Math.sqrt(Math.pow((this.x - x2), 2) + Math.pow((this.y - y2), 2));
+    //return this.x*this.y;
+  }
 }
