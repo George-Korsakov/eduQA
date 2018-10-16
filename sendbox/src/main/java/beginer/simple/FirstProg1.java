6fdbb454a4e7b2247fe7381d5525bfb90cae6927
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-17 02:30:41 +0300

    расчте расстояние между точками
    добавлен элемент случаности, потому что так интересней

--- sendbox/src/main/java/beginer/simple/FirstProg1.java
+++ sendbox/src/main/java/beginer/simple/FirstProg1.java
@@ -11,9 +11,14 @@ public class FirstProg1 {
     Point p1 = new Point(5, 7);
     Point p2 = new Point(8, 3);
 
-    // вызываем функцию расчета расстояние между точками и выводим результат вычислений
+    //  расчет расстояние между точками и выводим результат вычислений
     System.out.println("Расстояние моежду nочками " + "(" + p1.x + ", " + p1.y + ") и (" + p2.x + ", "+ p2.y + ") равнo " + p1.distance(p2.x, p2.y));
-    // пример вызова функции с неявным преобразованием типов
+
+    // рассчет растояения между точками для псевдослучайных значений
+    Point start = new Point ( (1 + (int)(Math.random() * 999)),(1 + (int)(Math.random() * 999)) );
+    Point end = new Point ( (1 + (int)(Math.random() * 999)),(1 + (int)(Math.random() * 999)) );
+    System.out.println ("Расстоение между случайнми точка в матрице 999х999 равно " + start.distance(end.x,end.y) );
+
   }
 
   public static void hello(String somebody) {
