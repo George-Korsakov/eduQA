6526302bf002036858c3f2c52497b2505637b96a
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-18 23:45:43 +0300

    добавлен негативный тест на граничное значение типа данных, что бы посмотреть сообщение об ошибке

---
+++ sendbox/src/test/java/beginer/simple/PointTest4.java
@@ -0,0 +1,18 @@
+package beginer.simple;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest4 {
+  @Test
+  public void testPointExp()
+  {
+    // для воспроизведения ошибки с граничным значении
+    Point p1 = new Point(1, -1);
+    Point p2 = new Point(250*Math.pow(10, 485), -1);
+    Assert.assertEquals(p1.distance(p2.x, p2.y), 1.0);
+
+  }
+
+
+}
