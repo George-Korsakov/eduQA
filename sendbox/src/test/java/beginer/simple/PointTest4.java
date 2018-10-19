f151aff2e3318d40d0661a1ad9db40a643f00f16
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-19 20:26:29 +0300

    тест после обовления

--- sendbox/src/test/java/beginer/simple/PointTest4.java
+++ sendbox/src/test/java/beginer/simple/PointTest4.java
@@ -8,9 +8,9 @@ public class PointTest4 {
   public void testPointExp()
   {
     // для воспроизведения ошибки с граничным значении
-    Point p1 = new Point(1, -1);
-    Point p2 = new Point(250*Math.pow(10, 485), -1);
-    Assert.assertEquals(p1.distance(p2.x, p2.y), 1.0);
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(250*Math.pow(10, 485), 1);
+    Assert.assertEquals(p1.distance(p2.x, p2.y), 1);
 
   }
 
