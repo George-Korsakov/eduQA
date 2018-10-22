0d086389204ce7f7b576b52c5a3f301b2c96e10b
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-22 21:43:49 +0300

    перенес вспомогательные методы создания контакта в общий базовый класс
    перенос вспомогательных методов в общий базовый класс
    добавлены комментарии

--- web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,101 +1,16 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Test;
 
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.Select;
-
-public class ContactCreationTests {
-  private WebDriver wd;
-
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-//    wd = new ChromeDriver();
-//      wd = new InternetExplorerDriver();
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class ContactCreationTests extends TestBase {
 
   @Test
-  public void testContactCreation() throws Exception {
+  public void testContactCreation()  {
 
     initContactCreation();
     fillContactForm(new ContactData("NameTest6", "MiddleNameTest7", "LastNameTest7", "NickNameTest7", "C:\\Temp\\04.jpg", "Test123", "CoTest", "Russian, Moscow, Red Square, 1", "+74951230007", "test1@test.test", "5", "September", "1987", "TestGroup2"));
     submitContactCreation();
     retutnHomePage();
   }
-
-  private void retutnHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  private void submitContactCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFname());
-    wd.findElement(By.name("middlename")).sendKeys(contactData.getMname());
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLname());
-    wd.findElement(By.name("nickname")).sendKeys(contactData.getNname());
-    wd.findElement(By.name("photo")).sendKeys(contactData.getPhotoPathToFile());
-    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("home")).sendKeys(contactData.getPhoneNumHome());
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getbDay());
-    wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getbMonth());
-    wd.findElement(By.name("byear")).sendKeys(contactData.getbYear());
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getChosenGroup());
-    //wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Group:'])[1]/following::option[6]")).click();
-  }
-
-  private void initContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
 
 }
--- web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -18,6 +19,7 @@ public class TestBase {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
+    // вызов драйвера, переход на страницу и авторизация
     wd = new FirefoxDriver();
 //  wd = new ChromeDriver();
 //  wd = new InternetExplorerDriver();
@@ -27,6 +29,8 @@ public class TestBase {
     login("admin", "secret");
   }
 
+
+
   private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
@@ -34,28 +38,9 @@ public class TestBase {
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
-
+  // возврат на страницу списка групп
   protected void returnToGroupPage() {
     wd.findElement(By.linkText("Logout")).click();;
-  }
-
-  protected void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  protected void fillGroupForm(GroupDate groupDate) {
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupDate.getGroupName());
-    wd.findElement(By.name("group_header")).sendKeys(groupDate.getGroupHeader());
-    wd.findElement(By.name("group_footer")).sendKeys(groupDate.getGroupCommmet());
-  }
-
-  protected void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  protected void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
@@ -82,12 +67,65 @@ public class TestBase {
     }
   }
 
+  // подтверждение оперции в форме группы
+  protected void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+  // заполнение полей формы группы
+  protected void fillGroupForm(GroupDate groupDate) {
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupDate.getGroupName());
+    wd.findElement(By.name("group_header")).sendKeys(groupDate.getGroupHeader());
+    wd.findElement(By.name("group_footer")).sendKeys(groupDate.getGroupCommmet());
+  }
+  // создание новой группы вызов формы
+  protected void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+  // переход к списку групп
+  protected void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
+  // удаление выбранной группы
   protected void deleteSelectedGroups() {
     wd.findElement(By.name("delete")).click();
   }
-
+  // выбор группы для удаления
   protected void selectGroup() {
     wd.findElement(By.name("selected[]")).click();
+  }
+  // возврат на основную страницу
+  protected void retutnHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+  // подтверждение действия в форме контакта
+  protected void submitContactCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+  // заполнение полей формы контакта
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFname());
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMname());
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLname());
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNname());
+    wd.findElement(By.name("photo")).sendKeys(contactData.getPhotoPathToFile());
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).sendKeys(contactData.getPhoneNumHome());
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getbDay());
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getbMonth());
+    wd.findElement(By.name("byear")).sendKeys(contactData.getbYear());
+    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getChosenGroup());
+    // альтернативный сопособ выбора группы
+    //wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Group:'])[1]/following::option[6]")).click();
+  }
+  // добавление нового контакта вызов формы
+  protected void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
   }
 }
 
