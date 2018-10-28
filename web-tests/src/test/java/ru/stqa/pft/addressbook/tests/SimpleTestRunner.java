1c5a9d39488e68fbe954babe459560cc3d801c22
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-10-29 01:17:28 +0300

    тест

--- web-tests/src/test/java/ru/stqa/pft/addressbook/tests/SimpleTestRunner.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/tests/SimpleTestRunner.java
@@ -7,8 +7,14 @@ public class SimpleTestRunner  extends TestBase {
 
     @Test
             public void testEmpty(){
-     System.out.println("Тут будет какой то тест !");
-      app.getGroupHelper().returnToGroupPage();
+     System.out.println("Последовательное выполнение тестов !");
+
+     GroupCreationTests step1 = new GroupCreationTests();
+     ContactCreationTests step2 = new ContactCreationTests();
+     ContactModificationTests step3 = new ContactModificationTests();
+     ContactDelitionTests step4 = new ContactDelitionTests();
+     GroupModificationTest step5 = new GroupModificationTest();
+     GroupDelitionTests step6 = new GroupDelitionTests();
   }
 
 
