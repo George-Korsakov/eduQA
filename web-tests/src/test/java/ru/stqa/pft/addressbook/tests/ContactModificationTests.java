d4d7bc19cf4127f3bd9dfb714b2bb7452a2bc1c4
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-11-08 23:40:08 +0300

    тест
    изменена проверка в тесте модификации контактов

--- web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -5,6 +5,9 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.ContactShortData;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class ContactModificationTests extends TestBase {
 
   @Test
@@ -15,16 +18,26 @@ public class ContactModificationTests extends TestBase {
     if(! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createShortContact(new ContactShortData("NewTest"+ r,  "LastNameTest"));
     }
-    int before = app.getContactHelper().getContactCount();
+    //int before = app.getContactHelper().getContactCount();
+    List<ContactShortData> before = app.getContactHelper().getContactList();
     // редактирование полей первого контакта в списке
-    app.getContactHelper().selectContact(before-1);
+    app.getContactHelper().selectContact(before.size()-1);
+    ContactShortData contact = new ContactShortData("NameTestEdit", "LastNameTestEdit");
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("NameTest4"+r, "MiddleNameTest0e", "LastNameTest0e"+r, "NickNameTest0e", "C:\\Temp\\04.jpg", "Test edit", "CoTest", "Russian, Spb, Nevskaya str, 12", "+78121230907", "test1@test.test", "25", "November", "1987", null), false);
+    app.getContactHelper().fillShortContactForm(contact);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().retutnHomePage();
-    int after = app.getContactHelper().getContactCount();
+    //int after = app.getContactHelper().getContactCount();
+    List<ContactShortData> after = app.getContactHelper().getContactList();
     // проверка числа группы в списке до и после
-    Assert.assertEquals(before, after);
+    Assert.assertEquals(before.size(), after.size());
+
+    // проверка сравнением списков
+    before.remove(before.size()-1);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+
     app.getNavigationHelper().gotoExit();
 
   }
