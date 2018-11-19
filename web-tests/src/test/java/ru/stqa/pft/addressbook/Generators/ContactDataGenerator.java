3de944657b557987c8c6efc993015f4257679c57
George-Korsakov <44112363+george-korsakov@users.noreply.github.com>
2018-11-19 22:02:54 +0300

    Реализованы генераторы групп и контактов. Результат сохраняется в csv файл. Добавлена библиотека JCommander для параметризации запуска из командной строки.

--- web-tests/src/test/java/ru/stqa/pft/addressbook/Generators/ContactDataGenerator.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/Generators/ContactDataGenerator.java
@@ -4,7 +4,6 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactShortData;
-import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,8 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-
-  @Parameter(names  = "-c", description = "Contact count" )
+  // объявление переменных дял парметров и задание их опсиания
+  @Parameter(names = "-c", description = "Contact count")
   public int count;
 
   @Parameter(names = "-f", description = "Target file")
@@ -25,6 +24,7 @@ public class ContactDataGenerator {
   public static void main(String[] args) throws IOException {
     ContactDataGenerator generator = new ContactDataGenerator();
     JCommander jCommander = new JCommander(generator);
+    // если параметр задан не верно, то выводит текст в консоль с описанием параметров
     try {
       jCommander.parse(args);
     } catch (ParameterException ex) {
@@ -33,27 +33,25 @@ public class ContactDataGenerator {
     }
     generator.run();
   }
-
+// запуск генератора из двух частей: созданик данных, сохранение в файл.
   private void run() throws IOException {
     List<ContactShortData> contacts = generateContacts(count);
     save(contacts, new File(file));
   }
-
-  private  void save(List<ContactShortData> contacts, File file) throws IOException {
+// сохранение в файл
+  private void save(List<ContactShortData> contacts, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
-    for (ContactShortData contact : contacts){
-      writer.write(String.format("%s;%s;%s;%s;%s;\n", contact.getFname(),contact.getLname(),contact.getPhoneNumHome(),contact.getEmail(), contact.getAddress() ));
+    for (ContactShortData contact : contacts) {
+      writer.write(String.format("%s;%s;%s;%s;%s;\n", contact.getFname(), contact.getLname(), contact.getPhoneNumHome(), contact.getEmail(), contact.getAddress()));
     }
   }
-
-  private  List<ContactShortData> generateContacts(int count) {
-  List<ContactShortData> contacts = new ArrayList<ContactShortData>();
-    int r = (int)(Math.random()*1000000);
-    for (int i = 0; i < count; i++){
-      contacts.add(new ContactShortData().withFname(String.format("FName %s", r+i)).withLname(String.format("LName %s", r+i))
-              .withPhoneNumHome(String.format("%s",r*i)).withEmail(String.format("%s-@test.t",r-i))
-              .withAddress(String.format("Ru, Msc, Main str, %s", i)));
+// генерация данных
+  private List<ContactShortData> generateContacts(int count) {
+    List<ContactShortData> contacts = new ArrayList<ContactShortData>();
+    int r = (int) (Math.random() * 1000000);
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactShortData().withFname(String.format("FName %s", r + i)).withLname(String.format("LName %s", r + i)).withPhoneNumHome(String.format("%s", r * i)).withEmail(String.format("%s-@test.t", r - i)).withAddress(String.format("Ru, Msc, Main str, %s", i)));
     }
     return contacts;
   }
--- web-tests/src/test/java/ru/stqa/pft/addressbook/Generators/GroupDataGenerator.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/Generators/GroupDataGenerator.java
@@ -21,45 +21,41 @@ public class GroupDataGenerator {
 
 
   // функция генерации тестовых данных принмиате арагументы: целое число - кол-во групп, относительный путь к файлу в который будут записаны данные
-    public static void main(String[] args) throws IOException {
-      GroupDataGenerator generator = new GroupDataGenerator();
-      JCommander jCommander = new JCommander(generator);
-      try {
-        jCommander.parse(args);
-      } catch (ParameterException ex) {
-        jCommander.usage();
-        return;
-      }
-      generator.run();
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
 
-      /*int count = Integer.parseInt(args[0]);
-      File file = new File(args[1]);
-    List<GroupDate> groups = generateGroups(count);
-    save(groups, file);*/
   }
 
   private void run() throws IOException {
     List<GroupDate> groups = generateGroups(count);
     save(groups, new File(file));
-    }
+  }
 
   // запись данных в файл в формате csv
-  private  void save(List<GroupDate> groups, File file) throws IOException {
+  private void save(List<GroupDate> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
-  Writer writer = new FileWriter(file);
-  for (GroupDate group: groups){
-    writer.write(String.format("%s;%s;%s;\n", group.getGroupName(), group.getGroupHeader(), group.getGroupCommmet()));
+    Writer writer = new FileWriter(file);
+    for (GroupDate group : groups) {
+      writer.write(String.format("%s;%s;%s;\n", group.getGroupName(), group.getGroupHeader(), group.getGroupCommmet()));
+    }
+    // закрывает файл, важно !
+    writer.close();
   }
-  // закрывает файл, важно !
-  writer.close();
-  }
-// генерация данных для гурпп
-  private  List<GroupDate> generateGroups(int count) {
+
+  // генерация данных для гурпп
+  private List<GroupDate> generateGroups(int count) {
     List<GroupDate> groups = new ArrayList<GroupDate>();
-    int r = (int)(Math.random()*1000000);
-    for (int i = 0; i < count; i++){
-      groups.add(new GroupDate().withGroupName(String.format("TestName %s", i+r))
-              .withGroupHeader(String.format("Test %s", i+r)).withGroupCommmet(String.format("TestComment %s", i+r)));
+    int r = (int) (Math.random() * 1000000);
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupDate().withGroupName(String.format("TestName %s", i + r)).withGroupHeader(String.format("Test %s", i + r)).withGroupCommmet(String.format("TestComment %s", i + r)));
     }
     return groups;
   }
