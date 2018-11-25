531832315351625ec06855bc40755e4daa0ce421
George-Korsakov <44112363+george-korsakov@users.noreply.github.com>
2018-11-25 17:51:35 +0300

    Добавлен тест для подключения к БД MySQL. Подключена библиотека mysql:mysql-connector-java Проверена возможность подключения и получения данных.

---
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DvConnectionTest.java
@@ -0,0 +1,40 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupDate;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DvConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list ");
+      Groups groups = new Groups();
+      while (rs.next()){
+        groups.add(new GroupDate().withGroupId(rs.getInt("group_id")).withGroupName(rs.getString("group_name"))
+                .withGroupHeader(rs.getString("group_header")).withGroupCommmet(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
+      // Do someth
+      // ing with the Connection
+
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+
+  }
+}
