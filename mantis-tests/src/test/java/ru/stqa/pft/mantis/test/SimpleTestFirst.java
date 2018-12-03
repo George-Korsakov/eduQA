503ad15245ec467792af3b59098b498e4e6229f3
George-Korsakov <44112363+george-korsakov@users.noreply.github.com>
2018-12-03 23:52:59 +0300

    В проект добавлен новый модуль mantis , созданеа заготовка для тестов добавлен простейшй тест для проверки наличия поля ввода логина Задание №17

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/test/SimpleTestFirst.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.mantis.test;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class SimpleTestFirst extends TestBase{
+
+  @Test
+   public void testSimpleFirst() throws IOException {
+     app.init();
+    // простейшая проверка
+    app.isElementPresent(new By.ByName("username"));
+     app.stop();
+
+  }
+}
