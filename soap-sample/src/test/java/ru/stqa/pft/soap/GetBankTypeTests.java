41763115831bbe0a1932d8718875b4e4cd81fe94
George-Korsakov <44112363+george-korsakov@users.noreply.github.com>
2018-12-12 00:09:01 +0300

    Добавлен простой тест Soap работает только с junit

--- soap-sample/src/test/java/ru/stqa/pft/soap/GetBankTypeTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GetBankTypeTests.java
@@ -1,13 +1,20 @@
 package ru.stqa.pft.soap;
 
-import com.acme.mypackage.*;
-import org.testng.annotations.Test;
+import com.acme.mypackage.BLZService;
+import com.acme.mypackage.DetailsType;
+import com.acme.mypackage.GetBankType;
+import org.junit.Assert;
+import org.junit.Test;
+//import org.testng.annotations.Test;
 
 public class GetBankTypeTests {
 
   @Test
-  public void getBankTYpeTest(){
-   new getBankType().
+  public void getBankTypeTest(){
+    String t = String.valueOf(60060000);
+    DetailsType bank = new BLZService().getBLZServiceSOAP12PortHttp().getBank(t);
+    System.out.println(bank);
+    Assert.assertEquals(bank.getBezeichnung(), "DZ BANK");
   }
 
 }
