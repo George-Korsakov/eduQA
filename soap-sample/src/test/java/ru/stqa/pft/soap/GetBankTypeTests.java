e1788cd47aa51aed6211389fc1f8082e2b123d63
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-12-11 23:51:37 +0300

    тест
    добавлен простой тест SOAP не работает

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GetBankTypeTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.soap;
+
+import com.acme.mypackage.*;
+import org.testng.annotations.Test;
+
+public class GetBankTypeTests {
+
+  @Test
+  public void getBankTYpeTest(){
+   new getBankType().
+  }
+
+}
