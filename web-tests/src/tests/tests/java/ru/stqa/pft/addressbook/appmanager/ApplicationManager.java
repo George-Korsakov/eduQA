0f7942f64f9225011cda6c83022b595da54d0bfb
George-Korsakov <44112363+George-Korsakov@users.noreply.github.com>
2018-12-16 23:29:24 +0300

    реализована возможность работы с удаленным браузером

--- web-tests/src/tests/tests/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ web-tests/src/tests/tests/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -41,13 +41,19 @@ private final Properties properties;
     // вызов метода подключения к БД
     dbHelper = new DbHelper();
     // выбор браузера
-    if(browser.equals(BrowserType.FIREFOX)){
-    wd = new FirefoxDriver();
-  } else if (browser.equals(BrowserType.IE)) {
-    wd = new InternetExplorerDriver();
-  } else if(browser.equals(BrowserType.GOOGLECHROME)) {
-    wd = new ChromeDriver();
-  }
+    if("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      } else if (browser.equals(BrowserType.GOOGLECHROME)) {
+        wd = new ChromeDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+    }
 
       // ожидание пояявление элемента на странице 5c для подстраховки
     wd.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS);
