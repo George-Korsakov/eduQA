5911798b9ac385d107a56af788d1f1d3d75563f2
George-Korsakov <44112363+george-korsakov@users.noreply.github.com>
2018-12-16 15:19:38 +0300

    тест испорчен для проверки настройки Jenkins

--- sendbox/src/test/java/beginer/simple/PointTest2.java
+++ sendbox/src/test/java/beginer/simple/PointTest2.java
@@ -12,7 +12,7 @@ public class PointTest2 {
     Point r2 = new Point((1 + (int) (Math.random() * 999)), (1 + (int) (Math.random() * 999)));
     // альтернативнsq расчитывает ответ для сравнения
     double d = Math.sqrt(Math.pow((r1.x - r2.x), 2) + Math.pow((r1.y - r2.y), 2));
-    // проверка не равенства занчению (испорчен тест)
-    Assert.assertNotEquals(r1.distancep2p(r2.x, r2.y), d-15);
+    // проверка не равенства занчению
+    Assert.assertNotEquals(r1.distancep2p(r2.x, r2.y), d-1);
   }
 }
